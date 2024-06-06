a56411c5dd75d19d8986a4304442caf28518c561
David <107804273+Fokus1U@users.noreply.github.com>
2024-06-06 16:59:20 +0200

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="de-DE">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Hier wird immer die Metadescription (Was bei einem Googlefund gezeigt wird, erstellt) -->
+
+    <meta name="description" content="C. Jannen & M. Giersbach Dachdecker und Maler aus Witten. 
+    Als Dachdecker und Maler Fachbetrieb dürfen Sie von uns absolute Professionalität und solides Handwerk erwarten. 
+    Verlassen Sie sich auf unsere langjährige Erfahrung und kontaktieren Sie uns für eine unverbindliche Beratung.">
+    
+    <title>Jannen & Giersbach | Dachdecker und Maler aus Witten</title> <!-- Titel auf Seite anpassen-->
+
+    <link rel="icon" href="./medien/bilder/favicon_io/favicon.ico">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&family=Khula:wght@300;400;600;700;800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    
+    <!-- Immer die Schriftarten einfügen  -->
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./styles.css">
+
+
+</head>
+<body>  
+
+
+<!-- Hier beginnt eine responsive und erweiterbare Navigationsleiste. Erstellt mit Bootstrap und angepasst auf persönliche Bedürfnisse
+-->
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #A03232 ;">
+  <a class="navbar-brand container"> <img src="./medien/bilder/VektorlogoKlein.svg" alt="Logo der Dachdecker und Maler aus Witten Jannen und Giersbach" style="height: 25%; width: 25%; padding: 15px;" ></a>
+  <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button> -->
+
+  <div class="collapse navbar-collapse mr-auto flex-row-reverse justify-content-around container" id="navbarSupportedContent">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link me-2 roboto-regular navText" href="./index.html" style="font-size: 25px ;">Home</a>
+      </li>
+      <li class="nav-item  me-2 ">
+        <a class="nav-link roboto-regular navText" href="./seiten/team.html" style="font-size: 25px ;" >Team</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle roboto-regular navText" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-size: 25px ;">
+          Dachdecker
+        </a>
+        <div class="dropdown-menu " aria-labelledby="navbarDropdown">
+          <a class="dropdown-item roboto-light" href="./seiten/steildach.html">Steildach</a>
+          <a class="dropdown-item roboto-light" href="./seiten/flachdach.html">Flachdach</a>
+          <a class="dropdown-item roboto-light" href="./seiten/bauklempner.html">Bauklempner</a>
+          <a class="dropdown-item roboto-light" href="./seiten/schiefer.html">Schiefer</a>
+          <a class="dropdown-item roboto-light" href="./seiten/serviceUndReparatur.html">Service & Reparatur</a>
+          <a class="dropdown-item roboto-light" href="./seiten/balkonSanierung.html">Balkon-Sanierung</a>
+          <a class="dropdown-item roboto-light" href="./seiten/asbestSanierung.html">Asbest-Sanierung</a>
+          <a class="dropdown-item roboto-light" href="./seiten/wärmedämmung.html">Wärmedämmung</a>
+          <a class="dropdown-item roboto-light" href="./seiten/dachfenster.html">Dachfenster</a>
+          <a class="dropdown-item roboto-light" href="./seiten/fassaden.html">Fassaden</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle roboto-regular navText" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-size: 25px ;">
+          Maler
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item roboto-light" href="./seiten/vollwärmeschutz.html">Vollwärmeschutz</a>
+          <a class="dropdown-item roboto-light" href="./seiten/putzUndMalerarbeiten.html">Putz- & Malerarbeit</a>
+          <a class="dropdown-item roboto-light" href="./seiten/innenausbau.html">Innenausbau</a>
+          <a class="dropdown-item roboto-light" href="./seiten/renovierung.html">Renovierung</a>
+        </div>
+      </li>
+      <li class="nav-item  me-2">
+        <a class="nav-link roboto-regular navText" href="./seiten/referenzen.html" style="font-size: 25px ;" >Referenzen</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- Hier endet die Navigationsleiste -->
+
+<section id="hero">
+
+  
+    
+
+</section>
+
+<section id="welcome">
+  
+
+
+<div class="container">Diese Seite scheint nicht mehr zu existieren. Bitte navigieren Sie zu der <a href="index.html">Home</a> Seite</div>
+
+
+
+
+</section>
+
+<section id="callToAction">
+
+  <!-- Hier beginnt das Kontaktformular ab id contact-form -->
+
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center" style="background-color: #A0323212;">
+  <div class="container">
+    <div class="container">
+      <form id="contact-form">
+          <h2 style="font-size: 2rem;" class="khula-semibold">Kontaktieren Sie uns</h2>
+          <div class="container form-group">
+              <label for="name" style="font-size: 1.2rem; font-family:'Roboto', sans-serif;">Name:</label>
+              <input type="text" id="name" name="name" required>
+          </div>
+          <div class="container form-group">
+              <label for="email" style="font-size: 1.2rem; font-family:'Roboto', sans-serif;">Email:</label>
+              <input type="email" id="email" name="email" required>
+          </div>
+          <div class="container form-group ">
+              <label for="message" style="font-size: 1.2rem; font-family:'Roboto', sans-serif;">Nachricht:</label>
+              <textarea id="message" name="message" required></textarea>
+          </div>
+          <button type="submit" class="btn-more button">Senden</button>
+      </form>
+      <div id="response"></div>
+  </div>
+  <script src="script.js"></script>
+  </div>
+</div> 
+
+<!-- Ende Kontaktformular -->
+
+</section>
+
+<!-- Footer Bereich - Auch als Vorlage nutzbar -->
+
+
+<section id="footer">
+    <div class="container border-top">
+    <footer class="py-2">
+      <div class="row justify-content-between py-3 me-auto">
+        <div class="col-6 col-md-2 mb-3">
+          <h5 class="roboto-medium ">Rechtliches</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2 roboto-medium "><a href="./seiten/impressum.html" class="nav-link p-0 text-body-secondary">Impressum</a></li>
+            <li class="nav-item mb-2 roboto-medium "><a href="./seiten/datenschutz.html" class="nav-link p-0 text-body-secondary">Datenschutzerklärung</a></li>
+          </ul>
+        </div>
+    </div> 
+</section>
+
+
+<!-- Hier startet der All rights reserved Bereich -->
+
+
+<section id="rights" class="container">
+    <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+        <p class="fontLibre ">© 2023 | Erstellung durch <a href="https://byteopus.de" style="text-decoration: none; font-size: 1em ;" class="fontLibre nav-ref">Byteopus</a>, All rights reserved.</p>
+        <ul class="list-unstyled d-flex ">
+          <!-- <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+          <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+          <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li> -->
+        </ul>
+      </div>
+</section>
+<!-- Hier endet der All rights reserved Bereich -->
+
+
+<script src="./index.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+
+</html>
